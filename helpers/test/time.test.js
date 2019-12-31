c64cfda67cb8856bc8094fe4ec6862886764dfe1
Trey Decker <treydecker75@gmail.com>
2019-12-31 13:58:12 -0700

    added time.test.js

--- helpers/test/time.test.js
+++ helpers/test/time.test.js
@@ -4,15 +4,22 @@ const Time = require('../time');
 describe('Time', () => {
     it('Has method isWithinLastHour which returns true if the date provided is within an hour', () => {
         const currentTime = new Date();
-        assert.is
-
+        const response = Time.isWithinLastHour(currentTime);
+        assert.isBoolean(response, `Expected Boolean, got: ${response}`);
+        assert.isTrue(response, `Expected True, got: ${response}`);
     });
     
     it('Has method isWithinLastHour which returns false if the date provided is NOT within an hour', () => {
-
+        const lastYear = new Date(2018,01,01);
+        const response = Time.isWithinLastHour(lastYear);
+        assert.isBoolean(response, `Expected Boolean, got: ${response}`);
+        assert.isFalse(response, `Expected False, got: ${response}`);
     });
 
     it('Has a method timestamp, which returns a current Date/Time object', () => {
+        const timestamp = Time.timestamp();
+        const isDate = (date) => { return date instanceof Date && !isNaN(date.valueOf()) }
+        assert.isTrue(isDate(timestamp), `Expected True, got: ${isDate(timestamp)}`);
 
     });
 })
--- test.js
+++
@@ -1,5 +0,0 @@
-const { spawn } = require('child_process');
-
-const testFiles = [];
-
-spawn(`./node_modules/.bin/mocha ${testFiles} --exit`);
