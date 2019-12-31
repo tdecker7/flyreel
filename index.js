526ee32fd8d6fc5010bb9c6ea941ea113ff07796
Trey Decker <treydecker75@gmail.com>
2019-12-31 11:46:27 -0700

    added body parsing and external routes file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const metric = require('./routes/metric')
 
+app.use(express.json());
 app.get('/', (req, res) => res.send({ status: "ok" }));
+app.use('/metric', metric)
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
